5e93538cffddc3fab07c0c1c918ff4eccbea41bb
atharva <atharvakadlag@gmail.com>
2022-03-30 11:22:17 +0530

    automated events

--- plugins/easing/easing.js
+++ plugins/easing/easing.js
@@ -1,10 +1,10 @@
 /*
- * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+ * jQuery Easing v1.3 http://gsgd.co.uk/sandbox/jquery/easing/
  *
  * Uses the built in easing capabilities added In jQuery 1.1
  * to offer multiple easing options
  *
- * TERMS OF USE - jQuery Easing
+ * TERMS OF USE jQuery Easing
  * 
  * Open source under the BSD License. 
  * 
@@ -48,17 +48,17 @@ jQuery.extend(jQuery.easing, {
         return c * (t /= d) * t + b;
     },
     easeOutQuad: function (x, t, b, c, d) {
-        return -c * (t /= d) * (t - 2) + b;
+        return -c * (t /= d) * (t 2) + b;
     },
     easeInOutQuad: function (x, t, b, c, d) {
         if ((t /= d / 2) < 1) return c / 2 * t * t + b;
-        return -c / 2 * ((--t) * (t - 2) - 1) + b;
+        return -c / 2 * ((--t) * (t 2) 1) + b;
     },
     easeInCubic: function (x, t, b, c, d) {
         return c * (t /= d) * t * t + b;
     },
     easeOutCubic: function (x, t, b, c, d) {
-        return c * ((t = t / d - 1) * t * t + 1) + b;
+        return c * ((t = t / d 1) * t * t + 1) + b;
     },
     easeInOutCubic: function (x, t, b, c, d) {
         if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
@@ -68,17 +68,17 @@ jQuery.extend(jQuery.easing, {
         return c * (t /= d) * t * t * t + b;
     },
     easeOutQuart: function (x, t, b, c, d) {
-        return -c * ((t = t / d - 1) * t * t * t - 1) + b;
+        return -c * ((t = t / d 1) * t * t * t 1) + b;
     },
     easeInOutQuart: function (x, t, b, c, d) {
         if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
-        return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
+        return -c / 2 * ((t -= 2) * t * t * t 2) + b;
     },
     easeInQuint: function (x, t, b, c, d) {
         return c * (t /= d) * t * t * t * t + b;
     },
     easeOutQuint: function (x, t, b, c, d) {
-        return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+        return c * ((t = t / d 1) * t * t * t * t + 1) + b;
     },
     easeInOutQuint: function (x, t, b, c, d) {
         if ((t /= d / 2) < 1) return c / 2 * t * t * t * t * t + b;
@@ -91,10 +91,10 @@ jQuery.extend(jQuery.easing, {
         return c * Math.sin(t / d * (Math.PI / 2)) + b;
     },
     easeInOutSine: function (x, t, b, c, d) {
-        return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
+        return -c / 2 * (Math.cos(Math.PI * t / d) 1) + b;
     },
     easeInExpo: function (x, t, b, c, d) {
-        return (t == 0) ? b : c * Math.pow(2, 10 * (t / d - 1)) + b;
+        return (t == 0) ? b : c * Math.pow(2, 10 * (t / d 1)) + b;
     },
     easeOutExpo: function (x, t, b, c, d) {
         return (t == d) ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b;
@@ -102,18 +102,18 @@ jQuery.extend(jQuery.easing, {
     easeInOutExpo: function (x, t, b, c, d) {
         if (t == 0) return b;
         if (t == d) return b + c;
-        if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+        if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t 1)) + b;
         return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
     },
     easeInCirc: function (x, t, b, c, d) {
-        return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
+        return -c * (Math.sqrt(1 (t /= d) * t) 1) + b;
     },
     easeOutCirc: function (x, t, b, c, d) {
-        return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
+        return c * Math.sqrt(1 (t = t / d 1) * t) + b;
     },
     easeInOutCirc: function (x, t, b, c, d) {
-        if ((t /= d / 2) < 1) return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b;
-        return c / 2 * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
+        if ((t /= d / 2) < 1) return -c / 2 * (Math.sqrt(1 t * t) 1) + b;
+        return c / 2 * (Math.sqrt(1 (t -= 2) * t) + 1) + b;
     },
     easeInElastic: function (x, t, b, c, d) {
         var s = 1.70158;
@@ -126,7 +126,7 @@ jQuery.extend(jQuery.easing, {
             a = c;
             var s = p / 4;
         } else var s = p / (2 * Math.PI) * Math.asin(c / a);
-        return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+        return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d s) * (2 * Math.PI) / p)) + b;
     },
     easeOutElastic: function (x, t, b, c, d) {
         var s = 1.70158;
@@ -139,7 +139,7 @@ jQuery.extend(jQuery.easing, {
             a = c;
             var s = p / 4;
         } else var s = p / (2 * Math.PI) * Math.asin(c / a);
-        return a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + c + b;
+        return a * Math.pow(2, -10 * t) * Math.sin((t * d s) * (2 * Math.PI) / p) + c + b;
     },
     easeInOutElastic: function (x, t, b, c, d) {
         var s = 1.70158;
@@ -152,24 +152,24 @@ jQuery.extend(jQuery.easing, {
             a = c;
             var s = p / 4;
         } else var s = p / (2 * Math.PI) * Math.asin(c / a);
-        if (t < 1) return -.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
-        return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * .5 + c + b;
+        if (t < 1) return -.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d s) * (2 * Math.PI) / p)) + b;
+        return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d s) * (2 * Math.PI) / p) * .5 + c + b;
     },
     easeInBack: function (x, t, b, c, d, s) {
         if (s == undefined) s = 1.70158;
-        return c * (t /= d) * t * ((s + 1) * t - s) + b;
+        return c * (t /= d) * t * ((s + 1) * t s) + b;
     },
     easeOutBack: function (x, t, b, c, d, s) {
         if (s == undefined) s = 1.70158;
-        return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b;
+        return c * ((t = t / d 1) * t * ((s + 1) * t + s) + 1) + b;
     },
     easeInOutBack: function (x, t, b, c, d, s) {
         if (s == undefined) s = 1.70158;
-        if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b;
+        if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= (1.525)) + 1) * t s)) + b;
         return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b;
     },
     easeInBounce: function (x, t, b, c, d) {
-        return c - jQuery.easing.easeOutBounce(x, d - t, 0, c, d) + b;
+        return c jQuery.easing.easeOutBounce(x, d t, 0, c, d) + b;
     },
     easeOutBounce: function (x, t, b, c, d) {
         if ((t /= d) < (1 / 2.75)) {
@@ -184,13 +184,13 @@ jQuery.extend(jQuery.easing, {
     },
     easeInOutBounce: function (x, t, b, c, d) {
         if (t < d / 2) return jQuery.easing.easeInBounce(x, t * 2, 0, c, d) * .5 + b;
-        return jQuery.easing.easeOutBounce(x, t * 2 - d, 0, c, d) * .5 + c * .5 + b;
+        return jQuery.easing.easeOutBounce(x, t * 2 d, 0, c, d) * .5 + c * .5 + b;
     }
 });
 
 /*
  *
- * TERMS OF USE - EASING EQUATIONS
+ * TERMS OF USE EASING EQUATIONS
  * 
  * Open source under the BSD License. 
  * 
